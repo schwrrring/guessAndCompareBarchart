4d4ea671e0ed1a6815fd528a0227a26bdc174a5d
malte hildebrand <malte.hildebrand@gmail.com>
2017-08-25 16:00:44 +0200

    not yet working version d3.csv cant load data the way it is done at the moment

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-
 import * as Rx from '../node_modules/rxjs-es/Rx';
+import { draw } from './modules/barchart.js'; 
+import * as d3 from 'd3';
 document.write(
     '<script src="http://' + (location.host || 'localhost').split(':')[0] +
     ':35729/livereload.js?snipver=1"></' + 'script>'
   );
 console.log('hello lovely');
 
-const test = Rx.Observable.of(1,2,3, 4,5);
-test.subscribe(x=> console.log(x));
+d3.csv("../data/Bars.csv", function(data2){
+  //draw(data);
+	console.log(data2, 'data');
+});
+//const test = Rx.Observable.of(1,2,3, 4,5);
+//test.subscribe(x=> console.log(x));
 
 
 
---
+++ src/modules/barchart.js
@@ -0,0 +1,81 @@
+import * as d3 from 'd3';
+
+export function draw(data) {
+  // The draw function is called when the template first loads
+  var width = window.innerWidth,
+    height = window.innerHeight;
+  var svg = d3.select(document.body).append("svg").attr("width", width).attr("height", height),
+    margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom;
+
+  var tooltip = d3.select("body").append("div").attr("class", "toolTip");
+
+  var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+      y = d3.scaleLinear().rangeRound([height, 0]);
+
+  var colours = d3.scaleOrdinal()
+    .range(["#6F257F", "#CA0D59"]);
+  var barchart = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  console.log(data);
+
+  x.domain(data.bars.map(function (d) {
+    return d.area;
+  }));
+  y.domain([0, d3.max(data.bars, function (d) {
+    return d.value;
+    })]);
+    //
+  barchart.append("g")
+    .attr("class", "axis axis--x")
+    .attr("transform", "translate(0," + height + ")")
+    .call(axisBottom(x));
+
+  barchart.append("g")
+    .attr("class", "axis axis--y")
+    .call(axisLeft(y).ticks(5).tickFormat(function (d) {
+       return parseInt(d / 1000) + "K";
+    }).tickSizeInner([-width]))
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", "0.71em")
+      .attr("text-anchor", "end")
+      .attr("fill", "#5D6971")
+      .text("Average House Price - (£)");
+
+  barchart.selectAll(".bar")
+    .data(data.bars)
+    .enter().append("rect")
+    .attr("x", function (d) {
+      return x(d.area);
+    })
+    .attr("y", function (d) {
+      return y(d.value);
+    })
+    .attr("width", x.bandwidth())
+    .attr("height", function (d) {
+      return height - y(d.value);
+     })
+        .attr("fill", function (d) {
+            return colours(d.area);
+        })
+        .on("click", function(d) {
+						console.log(d);
+            popup.point(mouse(this)[0], mouse(this)[1]).html(d.word).draw();
+            event.stopPropagation();
+        });
+        //.call(setAttributes);
+        //.on("mousemove", function (d) {
+        //    tooltip
+        //        .style("left", event.pageX - 50 + "px")
+        //        .style("top", event.pageY - 70 + "px")
+        //        .style("display", "inline-block")
+        //        .html((d.area) + "<br>" + "£" + (d.value));
+        //})
+        //.on("mouseout", function (d) {
+        //    tooltip.style("display", "none");
+        //});
+}
