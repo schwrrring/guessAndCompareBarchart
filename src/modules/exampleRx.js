eadf5262c031fecc968e01bca1cc96e6e9de23b7
malte hildebrand <malte.hildebrand@gmail.com>
2017-08-25 21:27:13 +0200

    changing height of bar via dragging works

--- src/modules/exampleRx.js
+++ src/modules/exampleRx.js
@@ -11,14 +11,24 @@ export function drawableRect(){
 		.attr('id', 'dragrect');
 		
 		const mousemove = 	Rx.Observable
-		  .fromEvent(rect.node(), 'mousemove')
-		
+		  .fromEvent(document, 'mousemove')
+			const mouseup = 	Rx.Observable
+		  .fromEvent(rect.node(), 'mouseup')
+
 		Rx.Observable
 		  .fromEvent(rect.node(), 'mousedown')
-			.do( x => console.log(x, 'mousedown'))
+		//	.do( x => console.log(x, 'mousedown'))
 			.switchMap( () => mousemove )
-			.do( x => console.log(x, 'mousemove'))	
-			.subscribe();
+		//	.do( x => console.log(x, 'mousemove'))
+			.map( x => x.screenY  )
+			.do( x => console.log(x ,'screenY'))
+			.pairwise()
+			.do( x => console.log(x ,'paiwise'))
+			.map(([a,b]) =>  b -a )
+			.do( x => console.log(x ,'diff'))
+			.takeUntil(mouseup)
+			.repeat()	
+			.subscribe( x =>  rect.attr('height', parseInt(rect.attr('height')) + parseInt(x) ));
 }
 
 export const example = Rx.Observable.of(1,2,3);
