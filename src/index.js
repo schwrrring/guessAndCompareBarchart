bc1971d92c877efc654d9f430745c125777221bd
malte hildebrand <malte.hildebrand@gmail.com>
2017-08-25 21:07:54 +0200

    working version switchmap laeuft

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import * as Rx from '../node_modules/rxjs-es/Rx';
 import { draw } from './modules/barchart.js'; 
 import * as d3 from 'd3';
+import { drawableRect } from './modules/exampleRx.js'  
 document.write(
     '<script src="http://' + (location.host || 'localhost').split(':')[0] +
     ':35729/livereload.js?snipver=1"></' + 'script>'
   );
-console.log('hell flovely');
+console.log('hello lovely');
+
+drawableRect();
 
 d3.csv("../data/Bars.csv", function(data){
-  draw(data);
-	console.log(data, 'data');
+//  draw(data);
+//	console.log(data, 'data');
 });
 //const test = Rx.Observable.of(1,2,3, 4,5);
 //test.subscribe(x=> console.log(x));
--- src/modules/exampleRx.js
+++ src/modules/exampleRx.js
@@ -1,3 +1,24 @@
-import Rx from 'rxjs/Rx';
+import * as Rx from 'rxjs-es/Rx';
+import * as d3 from 'd3';
+
+export function drawableRect(){
+  const rect = d3.select('body')
+		.append('svg')
+		.append('rect')
+		.attr('fill', 'red')
+		.attr('width', 50)
+		.attr('height', 50)
+		.attr('id', 'dragrect');
+		
+		const mousemove = 	Rx.Observable
+		  .fromEvent(rect.node(), 'mousemove')
+		
+		Rx.Observable
+		  .fromEvent(rect.node(), 'mousedown')
+			.do( x => console.log(x, 'mousedown'))
+			.switchMap( () => mousemove )
+			.do( x => console.log(x, 'mousemove'))	
+			.subscribe();
+}
 
 export const example = Rx.Observable.of(1,2,3);
